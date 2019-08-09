0e936571b252524663b9ba3389126827a6a5fc71
李喆 <lizhe@dhcp-10-181-41-248.dynamic.eduroam.mwn.de>
2019-08-09 11:51:25 +0200

    play music tool

---
+++ music_play.ino
@@ -0,0 +1,90 @@
+//speaker connected to pin 3 and GND.
+
+// ____initialize music play tool____
+// change the melody and notes, you can play other songs.
+#include"pitches.h"
+
+// notes in the song 'Mukkathe Penne'
+int melody[] = {
+NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
+NOTE_G4, NOTE_C5, NOTE_AS4, NOTE_A4,                   
+NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
+NOTE_C4, NOTE_D4,0,                                 
+
+NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
+NOTE_G4, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4,      //29               //8
+NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
+NOTE_C4, NOTE_D4,0,                                       
+
+NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
+NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5,
+NOTE_C4, NOTE_D4, NOTE_DS4, NOTE_FS4, NOTE_D5, NOTE_C5,
+NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_AS4,             //58
+
+NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
+NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_G4,
+NOTE_A4, 0, NOTE_AS4, NOTE_A4, 0, NOTE_G4,
+NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, 0,
+
+NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
+NOTE_C4, NOTE_D4, 0,
+NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
+NOTE_C4, NOTE_D4
+};
+
+// note durations: 8 = quarter note, 4 = 8th note, etc.
+int noteDurations[] = {       //duration of the notes
+8,4,8,4,
+4,4,4,12,
+4,4,4,4,4,4,
+4,16,4,
+
+8,4,8,4,
+4,2,1,1,2,1,1,12,
+4,4,4,4,4,4,
+4,16,4,
+
+4,4,4,4,4,4,
+4,4,4,12,
+4,4,4,4,4,4,
+4,4,4,12,
+
+4,4,4,4,4,4,
+2,1,1,2,1,1,4,8,4,
+2,6,4,2,6,4,
+2,1,1,16,4,
+
+4,8,4,4,4,
+4,16,4,
+4,8,4,4,4,
+4,20
+};
+
+int speed=90;  //higher value, slower notes
+// ____initialize music play tool____
+
+// ____setup music play tool____
+void setup() {
+  Serial.begin(9600);
+}
+// ____setup music play tool____
+
+// music play tool function
+void playmusic(){
+  
+  for (int thisNote = 0; melody[thisNote]!=-1; thisNote++) {
+  
+  int noteDuration = speed*noteDurations[thisNote];
+  tone(3, melody[thisNote],noteDuration*.95);
+  Serial.println(melody[thisNote]);
+  
+  delay(noteDuration);
+  
+  noTone(3);
+  }
+}
+// music play tool function
+
+void loop() {
+  playmusic();
+}
