42a13e95840129ef2fe79259dba6dd743926592b
FruechteBini <blanz@mailbox.org>
2019-08-09 14:50:04 +0200

    added blowing microphone -fan functionality

--- micro.ino
+++ micro.ino
@@ -1,15 +1,27 @@
-/*int soundPin = A0;
+#define FAN 9
+
+int soundPin = A0;
 int sensorValue = 0;
  
 void setup ()
 {
 Serial.begin (9600);
+pinMode(FAN, OUTPUT);
 }
  
 void loop ()
 {
-sensorValue = analogRead (soundPin);
-Serial.println (sensorValue, DEC);
-delay (200);
+  
+  sensorValue = analogRead (soundPin);
+  Serial.println (sensorValue, DEC);
+  if(sensorValue < 150)
+  {
+    analogWrite(FAN, 50);
+  }
+  if(sensorValue > 150)
+  {
+    analogWrite(FAN, 255);
+    delay(2000);
+  }
+  
 }
-*/
