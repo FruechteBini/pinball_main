235054dded3e7b098794bfcfbe6ba720c48bb8ad
FruechteBini <blanz@mailbox.org>
2019-08-09 10:27:58 +0200

    added micro.ino - gets a value for volume level

---
+++ micro.ino
@@ -0,0 +1,15 @@
+/*int soundPin = A0;
+int sensorValue = 0;
+ 
+void setup ()
+{
+Serial.begin (9600);
+}
+ 
+void loop ()
+{
+sensorValue = analogRead (soundPin);
+Serial.println (sensorValue, DEC);
+delay (200);
+}
+*/
